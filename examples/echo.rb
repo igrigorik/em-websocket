eea6593dfb6a44bb9a19fcd946772b51d863d485
Ilya Grigorik <ilya@igvita.com>
2009-12-20 22:11:41 -0500

    browser <> server echo example

---
+++ examples/echo.rb
@@ -0,0 +1,7 @@
+require 'lib/em-websocket'
+
+EventMachine::WebSocket.start(:host => "0.0.0.0", :port => 8080) do |ws|
+  ws.onopen    { ws.send "Hello Client!"}
+  ws.onmessage { |msg| ws.send "Pong: #{msg}" }
+  ws.onclose   { puts "WebSocket closed" }
+end
