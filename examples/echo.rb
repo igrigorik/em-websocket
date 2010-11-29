e278f5a1c4db60be7485859520a069e08bfe1247
Martyn Loughran <me@mloughran.com>
2010-11-29 18:38:10 +0000

    Add onerror callback to the echo example

--- examples/echo.rb
+++ examples/echo.rb
@@ -4,4 +4,5 @@ EventMachine::WebSocket.start(:host => "0.0.0.0", :port => 8080, :debug => true)
   ws.onopen    { ws.send "Hello Client!"}
   ws.onmessage { |msg| ws.send "Pong: #{msg}" }
   ws.onclose   { puts "WebSocket closed" }
+  ws.onerror   { |e| puts "Error: #{e.message}" }
 end
