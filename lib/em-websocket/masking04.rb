4b53f25b1fe0096a715a771eeb6be398e9cdca1b
Martyn Loughran <me@mloughran.com>
2011-03-11 15:41:29 -0800

    Experimental MaskedString class to encapsulate data masking

--- lib/em-websocket/masking04.rb
+++ lib/em-websocket/masking04.rb
@@ -1,5 +1,28 @@
 module EventMachine
   module WebSocket
+    class MaskedString < String
+      def read_mask
+        raise "Too short" if bytesize < 4 # TODO - change
+        @masking_key = String.new(self[0..3])
+      end
+
+      def slice_mask
+        slice!(0, 4)
+      end
+
+      def getbyte(index)
+        super(index + 4) ^ @masking_key.getbyte(index % 4)
+      end
+
+      def getbytes(start_index, count)
+        data = ''
+        count.times do |i|
+          data << getbyte(start_index + i)
+        end
+        data
+      end
+    end
+
     module Masking04
       attr_accessor :masking_key
 
--- spec/unit/masking_spec.rb
+++ spec/unit/masking_spec.rb
@@ -26,3 +26,13 @@ describe 'EM::WebSocket::Masking04' do
     @m.unmask("\x00\x01\x00", 1).should == "\x00\x01\x01"
   end
 end
+
+describe EM::WebSocket::MaskedString do
+  it "should allow reading 4 byte mask and unmasking byte / bytes" do
+    t = EM::WebSocket::MaskedString.new("\x00\x00\x00\x01\x00\x01\x00\x01")
+    t.read_mask
+    t.getbyte(3).should == 0x00
+    t.getbytes(0, 4).should == "\x00\x01\x00\x00"
+    t.getbytes(1, 3).should ==     "\x01\x00\x00"
+  end
+end
