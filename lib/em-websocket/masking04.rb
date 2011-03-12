abe961aac36f0c019ac548a2e8f50e717cffdad6
Martyn Loughran <me@mloughran.com>
2011-03-11 19:32:06 -0800

    Removed unused masking api

--- lib/em-websocket/masking04.rb
+++ lib/em-websocket/masking04.rb
@@ -22,18 +22,5 @@ module EventMachine
         data
       end
     end
-
-    module Masking04
-      attr_accessor :masking_key
-
-      def unmask(data, position)
-        unmasked_data = ""
-        data.size.times do |i|
-          unmasked_data << (data.getbyte(i) ^ masking_key.getbyte(position % 4))
-          position += 1
-        end
-        unmasked_data
-      end
-    end
   end
 end
--- spec/unit/masking_spec.rb
+++ spec/unit/masking_spec.rb
@@ -1,32 +1,5 @@
 require 'helper'
 
-describe 'EM::WebSocket::Masking04' do
-  class MaskingContainer
-    include EM::WebSocket::Masking04
-  end
-  
-  before :each do
-    @m = MaskingContainer.new
-  end
-  
-  it "should allow setting mask key and unmasking arbitrary byte" do
-    @m.masking_key = "\x00\x00\x00\x00"
-    @m.unmask('a', 2).should == 'a'
-    
-    @m.masking_key = "\x00\x00\x00\x01"
-    @m.unmask("\x00", 0).should == "\x00"
-    @m.unmask("\x01", 0).should == "\x01"
-    @m.unmask("\x00", 3).should == "\x01"
-    @m.unmask("\x01", 3).should == "\x00"
-  end
-  
-  it "should allow unmasking arbitrary length strings given start pointer" do
-    @m.masking_key = "\x00\x00\x00\x01"
-    @m.unmask("\x00\x01\x00\x01", 0).should == "\x00\x01\x00\x00"
-    @m.unmask("\x00\x01\x00", 1).should == "\x00\x01\x01"
-  end
-end
-
 describe EM::WebSocket::MaskedString do
   it "should allow reading 4 byte mask and unmasking byte / bytes" do
     t = EM::WebSocket::MaskedString.new("\x00\x00\x00\x01\x00\x01\x00\x01")
